52987e058992c362c359a434afa1462efe1bdd00
Ryan Thompson <service@aiwebsystems.com>
2014-04-23 21:18:53 -0500

    ROUTES!

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+// Public Routes
+Route::get('/', 'PublicController@showWelcome');
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+// Admin Routes
+Route::get('/admin', 'AdminController@index');
