9616b93e3981d585cbf641f3afdcd99335ce4e86
Ryan Thompson <service@aiwebsystems.com>
2014-04-26 00:54:42 -0500

    Streams database

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'facets',
+			'database'  => 'streams',
 			'username'  => 'root',
 			'password'  => 'root',
 			'charset'   => 'utf8',
