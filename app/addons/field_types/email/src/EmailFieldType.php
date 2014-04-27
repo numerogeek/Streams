e439e9160630c7d362c7c7cee2889bb4f9f7ba0a
Ryan Thompson <service@aiwebsystems.com>
2014-04-26 23:49:49 -0500

    Update country, email, encrypted

---
+++ app/addons/field_types/email/src/EmailFieldType.php
@@ -0,0 +1,54 @@
+<?php namespace Addon\FieldType;
+
+use Streams\Addon\FieldTypeAbstract;
+
+class EmailFieldType extends FieldTypeAbstract
+{
+    /**
+     * The database column type this field type uses.
+     *
+     * @var string
+     */
+    public $columnType = 'string';
+
+    /**
+     * Field type version
+     *
+     * @var string
+     */
+    public $version = '1.1.0';
+
+    /**
+     * Initial field type validation requirements.
+     *
+     * @var array
+     */
+    public $validation = array('email');
+
+    /**
+     * Field type author information.
+     *
+     * @var array
+     */
+    public $author = array(
+        'name' => 'AI Web Systems, Inc.',
+        'url'  => 'http://aiwebsystems.com/'
+    );
+
+    /**
+     * Return the input used for forms.
+     *
+     * @return mixed
+     */
+    public function formInput()
+    {
+        return \Form::input(
+            'text',
+            $this->form_slug,
+            $this->value,
+            array(
+                'placeholder' => $this->getPlacehoder()
+            )
+        );
+    }
+}
---
+++ app/addons/field_types/user/language/swedish/user_lang.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['streams:user.name']                 = 'Användare';
+$lang['streams:user.restrict_group']       = 'Begränsa till grupp';
+$lang['streams:user.dont_restrict_groups'] = 'Begränsa inte till grupper';
+
+// Placeholders
+$lang['streams:user.placeholder'] = 'Type to search...';
