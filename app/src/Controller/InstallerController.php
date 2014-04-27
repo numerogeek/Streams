1bcbb9ef644db828b85addca4f4d0030f85c64ed
Ryan Thompson <service@aiwebsystems.com>
2014-04-27 01:24:43 -0500

    Clean up installer a bit with Docs

--- app/src/Controller/InstallerController.php
+++ app/src/Controller/InstallerController.php
@@ -4,7 +4,12 @@ use Illuminate\Routing\Controller;
 
 class InstallerController extends Controller
 {
-    protected $validSteps = array(
+    /**
+     * Installation steps
+     *
+     * @var array
+     */
+    protected $installationSteps = array(
         'step1',
         'step2',
         'step3',
@@ -13,43 +18,68 @@ class InstallerController extends Controller
         'complete'
     );
 
+    /**
+     * Create a new InstallerController instance
+     */
     public function __construct()
     {
-        // Check if the installer folder was removed
+        // Check if Streams is installed.
         if ($this->isInstalled() or !$this->installerExists()) {
             return \Redirect::to('/');
         }
 
-        $loader = new \Composer\Autoload\ClassLoader();
-        $loader->addPsr4('StreamsInstaller\\', base_path('installer/src'));
-        $loader->register();
+        $this->loader    = new \Composer\Autoload\ClassLoader();
+        $this->installer = new \StreamsInstaller\Installer;
+
+        $this->loader->addPsr4('StreamsInstaller\\', base_path('installer/src'));
+        $this->loader->register();
 
         \View::addNamespace('installer', base_path('installer/views'));
     }
 
+    /**
+     * Run an installer step.
+     *
+     * @param string $step
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function run($step = 'step1')
     {
         if (!$this->isValidStep($step)) {
             return \Redirect::to('/installer/step1');
         }
 
-        $installer = new \StreamsInstaller\Installer;
-        return $installer->{$step}();
+        return $this->installer->{$step}();
     }
 
+    /**
+     * Is this install step valid?
+     *
+     * @param null $step
+     * @return bool
+     */
     public function isValidStep($step = null)
     {
-        return in_array($step, $this->validSteps);
+        return in_array($step, $this->installationSteps);
     }
 
+    /**
+     * Does the installer directory exist?
+     *
+     * @return bool
+     */
     public function installerExists()
     {
-       return is_dir(base_path('installer'));
+        return is_dir(base_path('installer'));
     }
 
+    /**
+     * Is Streams installed?
+     *
+     * @return bool
+     */
     public function isInstalled()
     {
-        //@todo - add some efficient logic to determine if Streams is installed
-        return false;
+        return \Schema::hasTable('streams_streams');
     }
 }
