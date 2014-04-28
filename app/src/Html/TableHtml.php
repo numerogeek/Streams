8abe24b116aafe5bb31ad4c73c0a2ae81c9b17bd
Ryan Thompson <service@aiwebsystems.com>
2014-04-28 00:11:09 -0500

    Add TableHtml

---
+++ app/src/Html/TableHtml.php
@@ -0,0 +1,14 @@
+<?php namespace Streams\Html;
+
+use HtmlObject\Table;
+
+class TableHtml extends Table
+{
+    /**
+     * Create a new TableHtml instance.
+     */
+    public function __construct($collection)
+    {
+
+    }
+}
