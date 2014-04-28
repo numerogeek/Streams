e66444fcc16df4ca2b429ab1afcdee722c815318
Osvaldo Brignoni <obrignoni@aiwebsystems.com>
2014-04-28 11:20:18 -0400

    StreamSchemaColumnCreator

---
+++ app/src/Schema/StreamSchemaColumnCreator.php
@@ -0,0 +1,98 @@
+<?php namespace Streams\Schema;
+
+use Streams\Model\FieldAssignmentModel;
+
+class StreamSchemaColumnCreator
+{
+
+    /**
+     * Field assignment model
+     *
+     * @var \Streams\Model\FieldAssignmentModel
+     */
+    protected $fieldAssignment;
+
+    public function __construct(FieldAssignmentModel $fieldAssignment)
+    {
+        $this->fieldAssignment = $fieldAssignment;
+        $this->fieldType       = $fieldAssignment->getType();
+        $this->stream          = $fieldAssignment->stream;
+    }
+
+    public function getTable()
+    {
+        return $this->stream->prefix . $this->stream->slug;
+    }
+
+    /**
+     * Create the column
+     *
+     * @return boolean
+     */
+    public function createColumn()
+    {
+        // Check if the table exists
+        if (!\Schema::hasTable($this->getTable())) {
+            return false;
+        }
+
+        // Check if the column does not exist already to avoid "duplicate column" errors
+        if (\Schema::hasColumn($this->getTable(), $this->fieldtype->getColumnName())) {
+            return false;
+        }
+
+        \Schema::table(
+            $this->getTable(),
+            function ($table) use ($type, $field) {
+
+                $columnTypeMethod = camel_case($this->fieldType->columnType);
+
+                // -------------------------------------
+                // Constraint
+                // -------------------------------------
+                $constraint = 255;
+
+                $maxLength = $this->fieldAssigment->getSetting('max_length');
+
+                // First we check and see if a constraint has been added
+                if (isset($this->fieldType->columnConstraint) and $type->columnConstraint) {
+                    $constraint = $type->columnConstraint;
+
+                    // Otherwise, we'll check for a max_length field
+                } elseif (is_numeric($maxLength)
+                ) {
+                    $constraint = $maxLength;
+                }
+
+                // Only the string method cares about a constraint
+                if ($columnTypeMethod === 'string') {
+                    $column = $table->{$columnTypeMethod}($this->fieldType->getColumnName(), $constraint);
+                } else {
+                    $column = $table->{$columnTypeMethod}($this->fieldType->getColumnName());
+                }
+
+                // -------------------------------------
+                // Default
+                // -------------------------------------
+                $defaultValue = $this->fieldAssigment->getSetting('default_value');
+
+                if ($defaultValue and !in_array(
+                        $columnTypeMethod,
+                        array('text', 'longText')
+                    )
+                ) {
+                    $column->default($defaultValue);
+                }
+
+                // -------------------------------------
+                // Default to allow null
+                // -------------------------------------
+
+                $column->nullable();
+            }
+        );
+
+        return true;
+    }
+
+}
