7a65cf5b0322cd98e108a110530f6a8971114599
Ryan Thompson <service@aiwebsystems.com>
2014-04-28 01:37:43 -0500

    Whoops yes, remove users

--- app/addons/modules/users/lang/en/addon.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array(
-    'name'        => 'Addons',
-    'description' => 'Manage all of your Streams addons.',
-);
--- app/addons/modules/users/src/AddonsModule.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Addon\Module\Addons;
-
-use Streams\Addon\ModuleAbstract;
-
-class AddonsModule extends ModuleAbstract
-{
-}
