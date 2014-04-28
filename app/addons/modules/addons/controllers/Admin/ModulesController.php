ed6e149838a3bdfcadd6fb93144d7950c5a432ed
Ryan Thompson <service@aiwebsystems.com>
2014-04-28 00:11:51 -0500

    Addons module!

---
+++ app/addons/modules/addons/controllers/Admin/ModulesController.php
@@ -0,0 +1,31 @@
+<?php namespace Addon\Module\Addons\Controller\Admin;
+
+use Streams\Controller\AdminController;
+use Streams\Html\TableHtml;
+
+class ModulesController extends AdminController
+{
+    /**
+     * Create a new ModulesController instance.
+     *
+     * @param \Streams\Addon\ModuleManager $modules
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Display a table of all modules.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        $modules = \FieldType::getAll();
+
+        $table = new TableHtml($modules);
+
+        return \View::make('module.addons::admin/modules/index', compact('table'));
+    }
+}
