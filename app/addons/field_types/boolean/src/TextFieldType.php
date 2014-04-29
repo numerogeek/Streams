87cb318a9402e505a0910904f9830a8ea0d2c6b6
Ryan Thompson <service@aiwebsystems.com>
2014-04-29 01:39:33 -0500

    Add boolean FT

---
+++ app/addons/field_types/boolean/src/TextFieldType.php
@@ -0,0 +1,37 @@
+<?php namespace Addon\FieldType\Text;
+
+use Streams\Addon\FieldTypeAbstract;
+
+class BooleanFieldType extends FieldTypeAbstract
+{
+    /**
+     * The database column type this field type uses.
+     *
+     * @var string
+     */
+    public $columnType = 'integer';
+
+    /**
+     * Column constraint
+     *
+     * @var string
+     */
+    public $columnConstraint = 1;
+
+    /**
+     * Field type version
+     *
+     * @var string
+     */
+    public $version = '1.1.0';
+
+    /**
+     * Field type author information.
+     *
+     * @var array
+     */
+    public $author = array(
+        'name' => 'AI Web Systems, Inc.',
+        'url'  => 'http://aiwebsystems.com/',
+    );
+}
