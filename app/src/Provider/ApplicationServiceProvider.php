c90b17db6a5726b21a9a7fe81670860d9dcfa060
Ryan Thompson <service@aiwebsystems.com>
2014-04-29 22:02:50 -0500

    Create application services

---
+++ app/src/Provider/ApplicationServiceProvider.php
@@ -0,0 +1,40 @@
+<?php namespace Streams\Provider;
+
+use Illuminate\Support\ServiceProvider;
+use Streams\Support\Application;
+
+class ApplicationServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerApplication();
+
+        if (!\Application::isInstalled()) {
+            if (\Request::segment(1) !== 'installer') {
+                \Redirect::to('installer');
+            }
+        } else {
+            \Application::locate();
+        }
+    }
+
+    /**
+     * Register the application support class
+     */
+    protected function registerApplication()
+    {
+        $this->app->bind(
+            'streams.application',
+            function () {
+                return new Application;
+            }
+        );
+    }
+
+}
---
+++ app/src/Support/Application.php
@@ -0,0 +1,61 @@
+<?php namespace Streams\Support;
+
+use Illuminate\Support\Facades\Request;
+use Streams\Model\ApplicationModel;
+
+class Application
+{
+    /**
+     * The application reference.
+     *
+     * @var null
+     */
+    protected $appRef = null;
+
+    /**
+     * Create a new Application instance
+     */
+    public function __construct()
+    {
+        $this->apps = new ApplicationModel();
+    }
+
+    /**
+     * Check whether Streams is installed or not.
+     *
+     * @return bool
+     */
+    public function isInstalled()
+    {
+        return (!is_dir(base_path('installer')));
+    }
+
+    /**
+     * Locate and setup the application
+     *
+     * @return bool
+     */
+    public function locate()
+    {
+        $domain = trim(str_replace(array('http://', 'https://'), '', Request::root()), '/');
+
+        $app = $this->apps->findByDomain($domain);
+
+        $this->appRef = $app->reference;
+
+        return ($app);
+    }
+
+    /**
+     * Return the app reference
+     * @return string
+     */
+    public function getTablePrefix()
+    {
+        if (!$this->appRef) {
+            $this->locate();
+        }
+
+        return $this->appRef . '_';
+    }
+}
