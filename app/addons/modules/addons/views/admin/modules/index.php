8fab0c6ff8244ed1ed3350306acac726b5a38b87
Ryan Thompson <service@aiwebsystems.com>
2014-04-30 10:05:10 -0500

    Simple helper view

--- app/addons/modules/addons/views/admin/modules/index.php
+++ app/addons/modules/addons/views/admin/modules/index.php
@@ -1,1 +1,11 @@
-<?php echo $table; ?>
+<?php foreach ($modules as $module): ?>
+    <p>
+        <strong>
+            <?php echo $module->getName(); ?>
+        </strong>
+        &nbsp;
+        <?php echo \HTML::link('admin/addons/modules/install/'.$module->slug, 'Install'); ?>
+         -
+        <?php echo \HTML::link('admin/addons/modules/uninstall/'.$module->slug, 'Uninstall'); ?>
+    </p>
+<?php endforeach; ?>
