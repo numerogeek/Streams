c34eb7de40deb5e6d1eaa1c1bdcdd316c59db5b1
Ryan Thompson <service@aiwebsystems.com>
2014-04-30 22:29:56 -0500

    Streams theme

---
+++ app/addons/themes/streams/routes.php
@@ -0,0 +1,3 @@
+<?php
+
+Route::get('admin/test', 'Addon\Module\Test\Controller\TestController@index');
---
+++ app/addons/themes/streams/src/StreamsTheme.php
@@ -0,0 +1,13 @@
+<?php namespace Addon\Module\Test;
+
+use Streams\Addon\ThemeAbstract;
+
+class StreamsTheme extends ThemeAbstract
+{
+    /**
+     * This is an admin theme.
+     *
+     * @var bool
+     */
+    protected $isAdmin = true;
+}
