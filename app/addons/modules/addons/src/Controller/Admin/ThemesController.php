7e3fa5d81df4470bf607e51a862f01ff5ee2b236
Ryan Thompson <service@aiwebsystems.com>
2014-05-01 00:29:28 -0500

    Themes controller

---
+++ app/addons/modules/addons/src/Controller/Admin/ThemesController.php
@@ -0,0 +1,70 @@
+<?php namespace Addon\Module\Addons\Controller\Admin;
+
+use Streams\Controller\AdminController;
+
+class ThemesController extends AdminController
+{
+    /**
+     * Create a new ThemesController instance.
+     *
+     * @param \Streams\Addon\ThemeManager $themes
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Display a table of all themes.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        $themes = \Theme::sync();
+
+        return \View::make('module.addons::admin/themes/index', compact('themes'));
+    }
+
+    /**
+     * Install a theme.
+     *
+     * @param $slug
+     * @return bool
+     */
+    public function install($slug)
+    {
+        if (!$theme = \Theme::get($slug)) {
+            // Not found
+        }
+
+        if ($theme and \Theme::install($slug)) {
+            // Great
+        } else {
+            // Something went wrong - check logs
+        }
+
+        return \Redirect::to('admin/addons/themes');
+    }
+
+    /**
+     * Uninstall a theme.
+     *
+     * @param $slug
+     * @return bool
+     */
+    public function uninstall($slug)
+    {
+        if (!$theme = \Theme::get($slug)) {
+            // Not found
+        }
+
+        if ($theme and \Theme::uninstall($slug)) {
+            // Great
+        } else {
+            // Something went wrong - check logs
+        }
+
+        return \Redirect::to('admin/addons/themes');
+    }
+}
