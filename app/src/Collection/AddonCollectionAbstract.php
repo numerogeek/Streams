23db764e5f71035ab25f14bac557f4a9e6acc29b
Ryan Thompson <service@aiwebsystems.com>
2014-05-01 16:50:05 -0500

    Convenient method

--- app/src/Collection/AddonCollectionAbstract.php
+++ app/src/Collection/AddonCollectionAbstract.php
@@ -4,4 +4,19 @@ use Illuminate\Support\Collection;
 
 abstract class AddonCollectionAbstract extends Collection
 {
+    /**
+     * Find an addon by it's slug.
+     *
+     * @param $slug
+     * @return null
+     */
+    public function findBySlug($slug) {
+        foreach ($this->items as $item) {
+            if ($item->slug === $slug) {
+                return $item;
+            }
+        }
+
+        return null;
+    }
 }
