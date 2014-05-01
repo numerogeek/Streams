3397205df912bb98c7bd3652dcd9c39933edef4f
Ryan Thompson <service@aiwebsystems.com>
2014-04-30 20:13:05 -0500

    Plates is our Template engine.. Boom.

---
+++ app/src/Provider/TemplateServiceProvider.php
@@ -0,0 +1,35 @@
+<?php namespace Streams\Provider;
+
+use Illuminate\Support\ServiceProvider;
+use League\Plates\Engine;
+use League\Plates\Template;
+
+class TemplateServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerTemplate();
+    }
+
+    /**
+     * Register Plates for our template engine.
+     */
+    protected function registerTemplate()
+    {
+        $engine = new Engine('/');
+
+        $this->app->bind(
+            'streams.template',
+            function () use ($engine) {
+                return new Template($engine);
+            }
+        );
+    }
+
+}
