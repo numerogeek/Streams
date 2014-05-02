55aa28191b5c19494e34fa5852a56a14d376e92a
Ryan Thompson <service@aiwebsystems.com>
2014-05-02 02:24:04 -0500

    Setup environment

--- app/src/Manager/ThemeManager.php
+++ app/src/Manager/ThemeManager.php
@@ -8,4 +8,22 @@ class ThemeManager extends AddonManagerAbstract
      * @var string
      */
     protected $folder = 'themes';
+
+    /**
+     * Get the active admin theme.
+     *
+     * @return null
+     */
+    public function getAdminTheme()
+    {
+        $themes = $this->getAll();
+
+        foreach ($themes as $theme) {
+            if ($theme->isAdmin() and $theme->isInstalled() and $theme->isEnabled()) {
+                return $theme;
+            }
+        }
+
+        return null;
+    }
 }
