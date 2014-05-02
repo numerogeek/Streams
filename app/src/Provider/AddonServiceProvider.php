3d88f03950fd39fbd0c4b6ded492baaee0ee10f8
Ryan Thompson <service@aiwebsystems.com>
2014-05-01 19:28:40 -0500

    Simplify

--- app/src/Provider/AddonServiceProvider.php
+++ app/src/Provider/AddonServiceProvider.php
@@ -1,14 +1,9 @@
 <?php namespace Streams\Provider;
 
 
+use Illuminate\Support\Str;
 use Composer\Autoload\ClassLoader;
 use Illuminate\Support\ServiceProvider;
-use Streams\Addon\BlockManager;
-use Streams\Addon\ExtensionManager;
-use Streams\Addon\FieldTypeManager;
-use Streams\Addon\ModuleManager;
-use Streams\Addon\TagManager;
-use Streams\Addon\ThemeManager;
 
 class AddonServiceProvider extends ServiceProvider
 {
@@ -19,101 +14,29 @@ class AddonServiceProvider extends ServiceProvider
     {
         $loader = new ClassLoader;
 
-        $this->registerBlocks($loader);
-        $this->registerExtensions($loader);
-        $this->registerFieldTypes($loader);
-        $this->registerModules($loader);
-        $this->registerTags($loader);
-        $this->registerThemes($loader);
+        $this->registerAddons('blocks', $loader);
+        $this->registerAddons('extensions', $loader);
+        $this->registerAddons('field_types', $loader);
+        $this->registerAddons('modules', $loader);
+        $this->registerAddons('tags', $loader);
+        $this->registerAddons('themes', $loader);
     }
 
     /**
-     * Register Blocks
+     * Register addons.
      */
-    public function registerBlocks($loader)
+    public function registerAddons($type, $loader)
     {
+        $singular = Str::singular($type);
+        $manager  = 'Streams\Addon\\' . Str::studly($singular) . 'Manager';
+
         $this->app->singleton(
-            'streams.blocks',
-            function () use ($loader) {
-                return new BlockManager($loader);
+            'streams.' . $type,
+            function () use ($loader, $manager) {
+                return new $manager($loader);
             }
         );
 
-        $this->app['streams.blocks']->register();
-    }
-
-    /**
-     * Register Extensions
-     */
-    public function registerExtensions($loader)
-    {
-        $this->app->singleton(
-            'streams.extensions',
-            function () use ($loader) {
-                return new ExtensionManager($loader);
-            }
-        );
-
-        $this->app['streams.extensions']->register();
-    }
-
-    /**
-     * Register field types
-     */
-    public function registerFieldTypes($loader)
-    {
-        $this->app->singleton(
-            'streams.fieldtypes',
-            function () use ($loader) {
-                return new FieldTypeManager($loader);
-            }
-        );
-
-        $this->app['streams.fieldtypes']->register();
-    }
-
-    /**
-     * Register Modules
-     */
-    public function registerModules($loader)
-    {
-        $this->app->singleton(
-            'streams.modules',
-            function () use ($loader) {
-                return new ModuleManager($loader);
-            }
-        );
-
-        $this->app['streams.modules']->register();
-    }
-
-    /**
-     * Register Tags
-     */
-    public function registerTags($loader)
-    {
-        $this->app->singleton(
-            'streams.tags',
-            function () use ($loader) {
-                return new TagManager($loader);
-            }
-        );
-
-        $this->app['streams.tags']->register();
-    }
-
-    /**
-     * Register Themes
-     */
-    public function registerThemes($loader)
-    {
-        $this->app->singleton(
-            'streams.themes',
-            function () use ($loader) {
-                return new ThemeManager($loader);
-            }
-        );
-
-        $this->app['streams.themes']->register();
+        $this->app['streams.' . $type]->register();
     }
 }
