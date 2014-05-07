4239dd89e74ec239c195bd9470e700555e433730
Ryan Thompson <service@aiwebsystems.com>
2014-05-07 02:11:54 -0500

    Favicon

--- app/addons/themes/streams/views/partials/metadata.php
+++ app/addons/themes/streams/views/partials/metadata.php
@@ -16,12 +16,12 @@
 <title>Test &bull; Title</title>
 
 <!-- Favicon -->
-<link rel="icon" type="image/x-icon" href="favicon.ico">
-<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
-<link rel="apple-touch-icon" href="favicon.ico" />
-<link rel="apple-touch-icon" sizes="72x72" href="favicon.ico" />
-<link rel="apple-touch-icon" sizes="114x114" href="favicon.ico" />
-<link rel="apple-touch-icon" sizes="144x144" href="favicon.ico" />
+<link rel="icon" type="image/x-icon" href="../../../favicon.ico">
+<link rel="shortcut icon" type="image/x-icon" href="../../../favicon.ico">
+<link rel="apple-touch-icon" href="../../../favicon.ico" />
+<link rel="apple-touch-icon" sizes="72x72" href="../../../favicon.ico" />
+<link rel="apple-touch-icon" sizes="114x114" href="../../../favicon.ico" />
+<link rel="apple-touch-icon" sizes="144x144" href="../../../favicon.ico" />
 
 <?php Assets::add('../../../css/app.css'); ?>
 <?php Assets::add('../../../js/modernizr.min.js'); ?>
