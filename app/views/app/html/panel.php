881785bbc1ad47efe2eb2bef213dac1b7473f7f9
Ryan Thompson <service@aiwebsystems.com>
2014-05-08 01:46:30 -0500

    Couple simple HTML views

---
+++ app/views/app/html/panel.php
@@ -0,0 +1,8 @@
+<section class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title">Panel title</h3>
+    </div>
+    <div class="panel-body">
+        <?php echo $content; ?>
+    </div>
+</section>
---
+++ app/views/app/html/table/table.php
@@ -0,0 +1,11 @@
+<table class="table table-striped">
+    <tbody>
+    <?php foreach ($items as $item): ?>
+        <tr>
+            <td>
+                <?php echo $item; ?>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
