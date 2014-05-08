0483128cf07574f9892bead497d69a0a9d6bf1c0
Ryan Thompson <service@aiwebsystems.com>
2014-05-07 21:58:07 -0500

    Correct the helper classes

---
+++ app/src/Support/EntryHelper.php
@@ -0,0 +1,16 @@
+<?php namespace Streams\Support;
+
+class EntryHelper
+{
+    /**
+     * Get an entry model class.
+     *
+     * @param $stream_slug
+     * @param $stream_namespace
+     * @return mixed
+     */
+    public function getModelClass($slug, $namespace)
+    {
+        return \Str::studly("Streams\Model\\_{$namespace}\\_{$namespace}_{$slug}_EntryModel");
+    }
+}
