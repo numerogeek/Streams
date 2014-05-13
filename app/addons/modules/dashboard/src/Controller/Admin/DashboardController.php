f33c0dcbd40a394fe730ae12b8685c288a650544
Ryan Thompson <service@aiwebsystems.com>
2014-05-13 03:35:24 -0500

    Clean

--- app/addons/modules/dashboard/src/Controller/Admin/DashboardController.php
+++ app/addons/modules/dashboard/src/Controller/Admin/DashboardController.php
@@ -1,15 +1,11 @@
 <?php namespace Addon\Module\Dashboard\Controller\Admin;
 
 use Streams\Controller\AdminController;
-use Streams\Ui\EntryTableUi;
 
 class DashboardController extends AdminController
 {
     public function index()
     {
-        $table = new EntryTableUi();
-        $model = new \Streams\Model\Addons\AddonsModulesEntryModel;
-
-        $table->make($model)->render();
+        return $this->template->render('module.dashboard::index');
     }
 }
--- app/addons/modules/dashboard/views/test.php
+++
@@ -1,6 +0,0 @@
-<?php $this->start('content'); ?>
-<h1>Welcome!</h1>
-<p>Hello <?php echo Sentry::getUser()->email; ?></p>
-<hr/>
-<?php echo $this->table; ?>
-<?php $this->end(); ?>
